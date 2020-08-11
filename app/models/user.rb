e4eca121b11af6a84ee171db3adb9aa883b81961
Tawasi51 <koutyoku1220satori@gmail.com>
2020-08-11 16:06:59 +0900

    ユーザー管理機能　修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :items
 
    with_options presence: true do
-    validates :nickname, presence: {message: "is invalid. Input required nickname."}
+    validates :nickname, presence: {with: /\A[ぁ-んァ-ン一-龥]/,message: "is invalid. Input required nickname."}
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
     validates :given_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
     validates :kana_family_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
