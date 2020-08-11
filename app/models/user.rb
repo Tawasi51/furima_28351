bce78e7367d5890e5f4aa06f1e0febf8717739d6
Tawasi51 <koutyoku1220satori@gmail.com>
2020-08-11 18:06:51 +0900

    ユーザー管理機能　修正５

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
 
    with_options presence: true do
     VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]*\z/
     validates :nickname
     validates :family_name, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' }
     validates :given_name, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' }
-    validates :kana_family_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :kana_given_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :kana_family_name, format: { with: VALID_KANA_NAME_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :kana_given_name, format: { with: VALID_KANA_NAME_REGEX, message: 'is invalid. Input full-width katakana characters.' }
     validates :birth_day
-    # validates :password, format: { with: presence: true, message: "is invalid. Input required password."}
-    validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]*\z/, message: 'is invalid. Input half-width English numbers and letters.' }
+    validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Input half-width English numbers and letters.' }
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
