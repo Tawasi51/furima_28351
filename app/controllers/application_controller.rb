d3b90507015c22ed23d77858cb3258994a683e04
Tawasi51 <koutyoku1220satori@gmail.com>
2020-08-11 14:55:06 +0900

    ログイン画面のパスの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :family_name, :given_name, :kana_family_name, :kana_given_name, :birth_day])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :family_name, :given_name, :kana_family_name, :kana_given_name, :birth_day])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :kana_family_name, :kana_given_name, :birth_day])
   end
 
