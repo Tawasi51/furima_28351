b4fa0a1bb36d09e5b2852d8df99f479cf0ddcf8e
Tawasi51 <koutyoku1220satori@gmail.com>
2020-08-17 17:53:46 +0900

    商品編集機能　修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,8 +25,4 @@ class Item < ApplicationRecord
     validates :day_id
   end
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #          :recoverable, :rememberable, :validate
 end
