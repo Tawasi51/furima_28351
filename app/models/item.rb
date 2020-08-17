73518c4ee852d160bd904de98bc8e813ee286ff1
Tawasi51 <koutyoku1220satori@gmail.com>
2020-08-17 17:07:41 +0900

    ルボコップ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,24 @@
 class Item < ApplicationRecord
-   extend ActiveHash::Associations::ActiveRecordExtensions
-   belongs_to :user
-   has_one :sale
-   has_one :address
-   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one :sale
+  has_one :address
+  has_one_attached :image
 
-   belongs_to_active_hash :category
-   belongs_to_active_hash :product_state
-   belongs_to_active_hash :delivery
-   belongs_to_active_hash :prefecture
-   belongs_to_active_hash :day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_state
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
 
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :prefecture
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  end
 
-   with_options presence: true do
-      validates :name
-      validates :text
-      validates :prefecture
-      validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range"}
-   end
-
-   with_options numericality: { greater_than: 1, message: "is out of setting range"} do
+  with_options numericality: { greater_than: 1, message: 'is out of setting range' } do
     validates :category_id
     validates :product_state_id
     validates :delivery_id
